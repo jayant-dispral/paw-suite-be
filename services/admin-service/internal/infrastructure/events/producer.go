0642466389ac6664f994ce4e19de402ca36a5983
jayant-issar <jayantissar23@gmail.com>
2026-01-27 08:32:36 +0530

    changed name to Brandmoniter

--- services/admin-service/internal/infrastructure/events/producer.go
+++ services/admin-service/internal/infrastructure/events/producer.go
@@ -9,32 +9,31 @@ import (
 	"github.com/segmentio/kafka-go"
 )
 
-
 type Producer struct {
 	Writer *kafka.Writer
 }
 
 func NewProducer(brokers []string, topic string) *Producer {
 	writer := &kafka.Writer{
-		Addr: kafka.TCP(brokers...),
-		Topic: topic,
+		Addr:     kafka.TCP(brokers...),
+		Topic:    topic,
 		Balancer: &kafka.LeastBytes{},
 	}
 
-	return  &Producer{
+	return &Producer{
 		Writer: writer,
 	}
 }
 
-func (p *Producer) SendBrandSearch(ctx context.Context, event domain.BrandSearchEvent) error {
+func (p *Producer) SendBrandSearch(ctx context.Context, event domain.BrandMoniterEvent) error {
 	eventInByte, err := json.Marshal(event)
 	if err != nil {
 		log.Printf("Could not convert event to json: %v", err)
-		return  err
+		return err
 	}
 
-	return  p.Writer.WriteMessages(ctx, kafka.Message{
-		Key: []byte(event.ProjectID),
+	return p.Writer.WriteMessages(ctx, kafka.Message{
+		Key:   []byte(event.ProjectID),
 		Value: eventInByte,
 	})
 }
