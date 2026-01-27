da188063f5a7900598ca64f9135413aa05c0e6ea
jayant-issar <jayantissar23@gmail.com>
2026-01-27 06:39:43 +0530

    added Brand search event struct

---
+++ shared/domain/brand_moniter.go
@@ -0,0 +1,10 @@
+package domain
+
+import "time"
+
+type BrandSearchEvent struct {
+	ProjectID   string    `json:"project_id"`
+	KeyWord     string    `json:"key_word"`
+	RequestedBy string    `json:"requested_by"`
+	TimeStamp   time.Time `json:"time_stamp"`
+}
